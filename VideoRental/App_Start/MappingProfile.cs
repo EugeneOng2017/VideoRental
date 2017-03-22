3a748630e23caa86b4a9f169a11af394d7ad0e57
Eugene Ong <eugeneong2013@gmail.com>
2017-03-22 14:14:10 +0800

    Movie Web Api and Delete customer

--- VideoRental/App_Start/MappingProfile.cs
+++ VideoRental/App_Start/MappingProfile.cs
@@ -14,6 +14,9 @@ namespace VideoRental.App_Start
         {
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+
+            Mapper.CreateMap<MovieDto, Movie>();
+            Mapper.CreateMap<Movie, MovieDto>();
         }
     }
 }
